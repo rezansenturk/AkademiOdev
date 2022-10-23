5362db0ebd46f8cad1f854eb9eda9c2ae7aede91
90535 <90535@DESKTOP-QSO81CA>
2022-10-23 22:24:35 +0300

    Add project files.

---
+++ AkademiOdev/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkademiOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 500; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
